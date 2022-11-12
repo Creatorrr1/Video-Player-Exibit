72e0680ee6478ab8e734afff4e8dc4adf5a4b4f3
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-11-12 23:56:47 +0000

    update 1

--- player.js
+++ player.js
@@ -6,7 +6,6 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-/* Build out functions */
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
